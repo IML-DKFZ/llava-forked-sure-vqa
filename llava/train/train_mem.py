0ec56e5f324a5405bee6b45eff031026e527a55d
Erkan <s426n@odcf-worker02.inet.dkfz-heidelberg.de>
2024-09-17 12:24:39 +0200

    Add full finetuning files

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -1,4 +1,6 @@
-from llava.train.train import train
+# from llava.train.train import train
+from train_full_finetuning import train
 
 if __name__ == "__main__":
-    train(attn_implementation="flash_attention_2")
+    # train(attn_implementation="flash_attention_2")
+    train()
